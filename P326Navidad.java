7e0cd7eaa334a86083e1a0354658914ccb85c70d
Joaquin0-0 <jopego02@xuqueralumnat.es>
2020-11-18 08:42:46 +0100

    Mi primer commit

---
+++ P326Navidad.java
@@ -0,0 +1,20 @@
+package P326Navidad;
+
+import java.util.Scanner;
+
+public class P326Navidad {
+
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+
+		int fechas;
+		
+		fechas=teclado.nextInt();
+		
+		int dia;
+		int mes;
+		
+		
+	}
+
+}
