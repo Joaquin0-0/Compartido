8160aacb4d8d09883ab0953b2513011d94038bd7
Diego5955 <72852377+Diego5955@users.noreply.github.com>
2020-11-18 11:05:36 +0100

    Add files via upload

--- P413ClarasyOscuras.java
+++ P413ClarasyOscuras.java
@@ -1,4 +1,5 @@
 package P413;
+<<<<<<< HEAD
 
 public class P413ClarasyOscuras {
 
@@ -8,3 +9,33 @@ public class P413ClarasyOscuras {
 	}
 
 }
+=======
+public class P413ClarasyOscuras {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int casos = s.nextInt();
+		int filas;
+		int columnas;
+		int blancas = 0;
+		int negras = 0;
+		int cantidad = 0;
+		int total;
+		for (int i = 0; i < casos; i++) {
+			filas = s.nextInt();
+			columnas = s.nextInt();
+			total = filas * columnas;
+			cantidad = total / 2;
+			blancas = cantidad;
+			negras = cantidad;
+			if (filas == 1 && columnas == 1) {
+				blancas = 1;
+				negras = 0;
+			}
+			if (total % 2 != 0) {
+				blancas = cantidad + 1;
+			}
+			System.out.println(blancas+ " "+ negras);
+		}
+	}
+}
+>>>>>>> 31af3e4... first commit
